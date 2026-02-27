c249ba2d640d5e986481cf7be8cf5944476d2a62
digitalommaurya <48587877+digitalommaurya@users.noreply.github.com>
2026-02-27 18:31:32 +0530

    Update wsgi.py
    new changes to wsgi file

--- crud_example/wsgi.py
+++ crud_example/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+from django.core.management import call_command  # <--- add this
 
 from django.core.wsgi import get_wsgi_application
 
