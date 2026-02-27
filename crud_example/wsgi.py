e0fed56cbf7cae452a55230cfbdf9dcc64355059
digitalommaurya <48587877+digitalommaurya@users.noreply.github.com>
2026-02-27 18:24:32 +0530

    Update wsgi.py

--- crud_example/wsgi.py
+++ crud_example/wsgi.py
@@ -12,5 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crud_example.settings')
+# Run migrations automatically (dangerous on each start!)
+call_command('migrate', interactive=False)
 
 application = get_wsgi_application()
